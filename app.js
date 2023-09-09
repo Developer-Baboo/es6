e544044c825d7fc7ac49c303c77a34c26213cd9b
Developer Baboo <babooheerani@gmail.com>
2023-09-09 23:59:07 +0500

    added some other functions

--- app.js
+++ app.js
@@ -226,10 +226,137 @@
         const result = data.filter((item) =>item.roll >10)
         console.log(result); */
 
+
+        //find() and findIndex() functions in es6
+
+        /* const data1 = [{name:"Baboo",roll:13},{name:"akash",roll:19}]
+        const result1 = data.find((item)=>item.name == "Baboo")
+        console.log(result1); */
+
+        //findIndex: array ka kis index pa match kr rha ha
+        /* const data = [{name:"Baboo",roll:13},{name:"akash",roll:19}]
+        const result = data.findIndex((item)=>item.name == "Baboo")
+        console.log(result);
+
+
+        
+ */
+
+        //Promise then cache block in ES6
+
+        /* const checkUser = (email)=>new Promise((resolve, reject)=>{
+            let username = "baboo@gmail.com";
+            if(username === email) resolve("User Found ! ")
+            else reject("user not found")
+        })
+        checkUser("1babsdfsdfoo@gmail.com")
+        .then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.log(err);
+        }); */
+
+
+
+        //startsWidth() endsWith() rest Parameters in ES6
+        
+        /* const data = "My name is Baboo"
+        const matched = data.startsWith("my")
+        alert(matched) */
+
+
+       /*  const data = "My name is Baboo"
+        const matched = data.endsWith("saurav")
+        alert(matched)
+ */
+
+
+        // /rest
+        /* const control = (name, roll, subject, ...rest)=>{
+            return {
+                name, 
+                roll,
+                subject,
+                ...rest
+            }
+        }
+        let extra = {
+            marks:"80%",
+            grade: "A",
+            date: new Date()
+        }
+        console.log(control("saurav",12, "maths", extra)); */
+
+        // Promise Try Catch Async Await in ES6
+
+        /* const required = (el)=>new Promise((resolve, reject)=>{
+            const value = el.value;
+            if(value.length === 0) reject("This is required!")
+            else resolve("Entry accepted")
+        });
+
+        const onBlur = async () =>{
+            try{
+                const input = document.querySelector("#input");
+                const result = await required(input)
+                alert(result);
+            }
+            catch(err)
+            {
+                input.style.border = "2px solid red";
+                input.placeholder = err;
+            }
+        } */
+        //sucess:resolve
+        //reject: failure
+
+
+        //Array() fill() from() push() keys() in ES6
+
+        // Array : 
+       /*  const data1 = Array(10);
+        console.log(data1); */
+
+        /* const data1 = Array(10);
+        data1[0] = "1";
+        data1[1] = "2";
+        console.log(data1); */
+
+        //Fill
+        /* const data = Array(10).fill("a")
+        // fill: array ke hr index pa a store hoga  
+        console.log(data);
+        data[1] = "aa";
+        data[2] = "ab";
+        data[3] = "cc";;
+        console.log(data); */
+
+
+        //push: add one by one
+        /* const data = Array()
+        data.push("abc1");
+        data.push("abc2");
+        data.push("abc3");
+        console.log(data);
+ */
+
+        //From
+        /* const data = Array.from('asdf');
+        console.log(data);  */
+
+       /*  const data = [
+            "123",
+            "321",
+        ]
+        const keys = data.keys();
+        for(let key of keys){
+            console.log(key);
+        } */
+
         
 
-
-
+      
 
 
 
